5896dcdb0ca2bec2cb88397f24bef26150758636
eliska <eliska.novotna@teskalabs.com>
2023-05-15 16:05:07 +0200

    mongo added

---
+++ Site/ASAB Maestro/Files/mongo/replica.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Get hostnames from environment variable
+IFS=',' read -r -a hostnames <<< "$HOSTNAMES"
+
+port=27017
+
+# The JavaScript file to execute
+script=/script/reconfigureReplicaSet.js
+
+# Read the JSON file
+newConfig=$(cat /script/newConfig.json)
+
+for hostname in "${hostnames[@]}"; do
+echo "Trying hostname $hostname"
+
+# Prepare the JavaScript command with the newConfig variable
+command="var newConfig = $newConfig; $(cat $script)"
+
+# Run the MongoDB shell command
+output=$(mongosh --host $hostname --port $port --eval "$command")
+
+# Print the output
+echo "$output"
+
+# Check the output for error or success messages
+if [[ $output == *"ERROR_NOT_PRIMARY"* ]]; then
+	echo "Notice: Not a primary node. Trying another one..."
+elif [[ $output == *"ERROR_RECONFIG"* ]]; then
+	echo "Error: Failed to reconfigure the replica set."
+	break
+elif [[ $output == *"SUCCESS_RECONFIG"* ]]; then
+	echo "Success: Replica set reconfigured."
+	break
+elif [[ $output == *"SUCCESS_INITIATE"* ]]; then
+	echo "Success: New replica set initiated."
+	break
+fi
+done
