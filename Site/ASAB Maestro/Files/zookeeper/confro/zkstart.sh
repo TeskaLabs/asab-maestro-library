e55c869132c5687ad6b3f67e2beb481282578ab6
Ales Teska <ales.teska@teskalabs.com>
2023-09-13 01:06:46 +0200

    Improvements.

--- Site/ASAB Maestro/Files/zookeeper/confro/zkstart.sh
+++ Site/ASAB Maestro/Files/zookeeper/confro/zkstart.sh
@@ -2,8 +2,10 @@
 
 set -e
 
-cp /confro/zoo.cfg /conf/zoo.cfg
+# Copy the configuration from the read-only site directory to the /conf
+cp /confro/zoo.cfg $ZOO_CONF_DIR/zoo.cfg
 
+# Create a "myid" file in the data directory
 if [[ ! -f "$ZOO_DATA_DIR/myid" ]]; then
     echo "${ZOO_MY_ID:-1}" > "$ZOO_DATA_DIR/myid"
 fi
