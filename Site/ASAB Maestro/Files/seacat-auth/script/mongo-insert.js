46658f4ff4b0efce06cb2521bf348a3775023f94
enovotna <eliska.novotna@teskalabs.com>
2024-05-21 17:24:28 +0200

    upsert each document, add "managed by maestro" tag

--- Site/ASAB Maestro/Files/seacat-auth/script/mongo-insert.js
+++ Site/ASAB Maestro/Files/seacat-auth/script/mongo-insert.js
@@ -35,36 +35,23 @@ function transform_collection(collectionName, data) {
 		record["_c"] = new Date()
 		record["_m"] = new Date()
 		record["_v"] = 1
+		record["managed_by"] = "maestro"
 	});
 
 	return data
 }
 
-/**
- * The function upsertSeaCatAuthCollections inserts or updates multiple collections in the "auth"
- * database using the provided data, and handles duplicate key errors.
- * @param data - The `data` parameter is an object that contains collections and their corresponding
- * data to be upserted into the "auth" database. Each key in the `data` object represents a collection
- * name, and the corresponding value is an array of documents to be inserted or updated in that
- * collection.
- */
-function insertSeaCatAuthCollections(data) {
+
+function upsertSeaCatAuthCollections(data) {
 	// seacat auth uses "auth" database
 	authDb = db.getSiblingDB( "auth" );
 	data.forEach(line => {
 		let collectionName = line[0]
 		const collection = authDb.getCollection(collectionName)
-		try {
-			res = collection.insertMany(line[1], {ordered: false})
-			print(`Insterted ${Object.keys(res.insertedIds).length} into ${collectionName} collection.`)
-		} catch (MongoBulkWriteError) {
-			if (MongoBulkWriteError.code === 11000) {
-				res = MongoBulkWriteError.result
-				print(`Insterted ${res.insertedCount} into ${collectionName} collection.`)
-			} else {
-				throw MongoBulkWriteError
-			}
-		}
+		line[1].forEach(document => {
+			print(document["_id"])
+			collection.updateOne({_id: document["_id"]}, { $set: document }, {upsert: true})
+		});
 	});
 }
 
@@ -92,7 +79,8 @@ function main() {
 				continue
 			}
 
-			if (!db.isMaster()?.isWritablePrimary ?? false) {  //The optional chaining operator (?.) will return undefined instead of causing an error if isMaster() returns null or undefined, or if isWritablePrimary does not exist on the object returned by isMaster(). The nullish coalescing operator (??) will return the value on its right-hand side if the value on its left-hand side is null or undefined.
+			if (!db.hello()?.isWritablePrimary ?? false) {  //The optional chaining operator (?.) will return undefined instead of causing an error if isMaster() returns null or undefined, or if isWritablePrimary does not exist on the object returned by isMaster(). The nullish coalescing operator (??) will return the value on its right-hand side if the value on its left-hand side is null or undefined.
+				// skip mongo instances that are not primary
 				continue
 			};
 		
@@ -105,7 +93,7 @@ function main() {
 			
 	
 			try {
-				insertSeaCatAuthCollections(data);
+				upsertSeaCatAuthCollections(data);
 			} catch (err) {
 				print("UNSUCCESSFUL_DATA_INSERT", err);
 				quit(1)
