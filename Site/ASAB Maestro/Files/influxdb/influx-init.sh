c645ae7934891ca625b74585a8310e9df5695fae
Jakub Schier <jakub.schier@teskalabs.com>
2024-10-17 20:19:34 +0200

    shuffle variables

--- Site/ASAB Maestro/Files/influxdb/influx-init.sh
+++ Site/ASAB Maestro/Files/influxdb/influx-init.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 # Wait for InfluxDB to start
-until curl -s http://{{INFLUXDB_URL}}:8086/health | grep -q '"status": "pass"'; do
+until curl -s http://{{INFLUXDB_HOSTNAME}}:8086/health | grep -q '"status": "pass"'; do
   echo "Waiting for InfluxDB to start..."
   sleep 1
 done
 
 # Get the bucket ID
-BUCKET_ID=$(curl -s -X GET http://{{INFLUXDB_URL}}:8086/api/v2/buckets -H 'Authorization: Token "{{INFLUXDB_TOKEN}}"' -H "Accept: application/json" | jq -r '.buckets[] | select(.name=="{{BUCKET_NAME}}") | .id')
+BUCKET_ID=$(curl -s -X GET http://{{INFLUXDB_HOSTNAME}}:8086/api/v2/buckets -H 'Authorization: Token "{{INFLUXDB_TOKEN}}"' -H "Accept: application/json" | jq -r '.buckets[] | select(.name=="{{DOCKER_INFLUXDB_INIT_BUCKET}}") | .id')
 # Get the org ID
-ORG_ID=$(curl -s -X GET http://{{INFLUXDB_URL}}:8086/api/v2/orgs -H 'Authorization: Token "{{INFLUXDB_TOKEN}}"' -H "Accept: application/json" | jq -r '.orgs[] | select(.name=="{{ORG_NAME}}") | .id')
+ORG_ID=$(curl -s -X GET http://{{INFLUXDB_HOSTNAME}}:8086/api/v2/orgs -H 'Authorization: Token "{{INFLUXDB_TOKEN}}"' -H "Accept: application/json" | jq -r '.orgs[] | select(.name=="{{DOCKER_INFLUXDB_INIT_ORG}}") | .id')
 
 # Get the user ID
-USER_ID=$(curl -s -X GET http://{{INFLUXDB_URL}}:8086/api/v2/users -H 'Authorization: Token "{{INFLUXDB_TOKEN}}"' -H "Accept: application/json" | jq -r '.users[] | select(.name=="{{USER_NAME}}") | .id')
+USER_ID=$(curl -s -X GET http://{{INFLUXDB_HOSTNAME}}:8086/api/v2/users -H 'Authorization: Token "{{INFLUXDB_TOKEN}}"' -H "Accept: application/json" | jq -r '.users[] | select(.name=="{{DOCKER_INFLUXDB_INIT_USERNAME}}") | .id')
 
 # Create the non-admin token using the InfluxDB API v2
-curl -X POST http://{{INFLUXDB_URL}}:8086/api/v2/authorizations \
+curl -X POST http://{{INFLUXDB_HOSTNAME}}:8086/api/v2/authorizations \
   -H "Authorization: Token {{INFLUXDB_TOKEN}}" \
   -H "Accept: application/json" \
   -H "Content-Type: application/json" \
